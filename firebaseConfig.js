03f2cdaf81cae30c09a0949bc3bca06a16bf72f7
Brendon Ng <brendonn8@gmail.com>
2023-08-11 17:09:00 -0700

    Offline play and online play supported (#3)

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from './secrets';
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const firebase_app = initializeApp(firebaseConfig);
+export const firestore_db = getFirestore(firebase_app);
+export const firebase_auth = getAuth(firebase_app);
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Guess from './Guess';
 import { MAX_GUESSES } from '../constants/gameConstants';
 import generateTargetWord from '../utils/generateWord';
 import { useDispatch, useSelector } from 'react-redux';
-import { addLoss, addWin, resetGuesses } from '../store';
+import { resetGuesses } from '../store';
+import { doc, getDoc, setDoc, runTransaction } from 'firebase/firestore';
+import { firestore_db } from '../../firebaseConfig';
+import { OFFLINE_USER } from '../constants/apiConstants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STATUS_ONGOING = 'ongoing';
 const STATUS_LOSS = 'loss';
@@ -20,15 +24,160 @@ function Game() {
     return state.guesses;
   });
 
+  const { userInfo } = useSelector((state) => state.user);
+  const addWin = useCallback(
+    async (guesses) => {
+      if (userInfo.uid === OFFLINE_USER) {
+        AsyncStorage.getItem('@scores').then((scoresJSON) => {
+          if (scoresJSON) {
+            const scoresData = JSON.parse(scoresJSON);
+            scoresData.gamesPlayed = scoresData.gamesPlayed + 1;
+            scoresData.gamesPlayed = scoresData.gamesPlayed + 1;
+            scoresData.curStreak = 0;
+            scoresData.gamesPlayed = scoresData.gamesPlayed + 1;
+            scoresData.gamesWon = scoresData.gamesWon + 1;
+            const newStreak = scoresData.curStreak + 1;
+            scoresData.curStreak = newStreak;
+            scoresData.largestStreak =
+              newStreak > scoresData.largestStreak
+                ? newStreak
+                : scoresData.largestStreak;
+            let newGuessDist = [...scoresData.guessDist];
+            newGuessDist[guesses - 1] = newGuessDist[guesses - 1] + 1;
+            scoresData.guessDist = newGuessDist;
+            AsyncStorage.setItem('@scores', JSON.stringify(scoresData)).then(
+              console.log('added win to local storage')
+            );
+          } else {
+            const scoresData = {
+              gamesPlayed: 1,
+              gamesWon: 1,
+              curStreak: 1,
+              largestStreak: 1,
+              guessDist: Array.from({ length: MAX_GUESSES }, (e, i) =>
+                i === guesses - 1 ? 1 : 0
+              ),
+            };
+            AsyncStorage.setItem('@scores', JSON.stringify(scoresData)).then(
+              console.log('Initialized Local Storage scores with a win')
+            );
+          }
+        });
+      } else {
+        const ref = doc(firestore_db, 'scores', userInfo.uid);
+        const snap = await getDoc(ref);
+        if (!snap.exists()) {
+          setDoc(doc(firestore_db, 'scores', userInfo.uid), {
+            gamesPlayed: 1,
+            gamesWon: 1,
+            curStreak: 1,
+            largestStreak: 1,
+            guessDist: Array.from({ length: MAX_GUESSES }, (e, i) =>
+              i === guesses - 1 ? 1 : 0
+            ),
+          });
+          console.log('Doc Created');
+        } else {
+          try {
+            await runTransaction(firestore_db, async (transaction) => {
+              const scoreDoc = await transaction.get(ref);
+              if (!scoreDoc.exists()) {
+                console.log(
+                  'ERROR: Scores document does not exit for this user'
+                );
+              }
+              const newGamesPlayed = scoreDoc.data().gamesPlayed + 1;
+              const newWins = scoreDoc.data().gamesWon + 1;
+              const newCurStreak = scoreDoc.data().curStreak + 1;
+              const newLargestStreak =
+                newCurStreak > scoreDoc.data().largestStreak
+                  ? newCurStreak
+                  : scoreDoc.data().largestStreak;
+              let newGuessDist = [...scoreDoc.data().guessDist];
+              newGuessDist[guesses - 1] = newGuessDist[guesses - 1] + 1;
+              transaction.update(ref, {
+                gamesPlayed: newGamesPlayed,
+                gamesWon: newWins,
+                curStreak: newCurStreak,
+                largestStreak: newLargestStreak,
+                guessDist: newGuessDist,
+              });
+            });
+            console.log('Transaction committed');
+          } catch (e) {
+            console.log('Transaction failed:', e);
+          }
+        }
+      }
+    },
+    [userInfo]
+  );
+
+  const addLoss = useCallback(async () => {
+    if (userInfo.uid === OFFLINE_USER) {
+      AsyncStorage.getItem('@scores').then((scoresJSON) => {
+        if (scoresJSON) {
+          const scoresData = JSON.parse(scoresJSON);
+          scoresData.gamesPlayed = scoresData.gamesPlayed + 1;
+          scoresData.curStreak = 0;
+          AsyncStorage.setItem('@scores', JSON.stringify(scoresData)).then(
+            console.log('added loss to local storage')
+          );
+        } else {
+          const scoresData = {
+            gamesPlayed: 1,
+            gamesWon: 0,
+            curStreak: 0,
+            largestStreak: 0,
+            guessDist: Array.from({ length: MAX_GUESSES }, () => 0),
+          };
+          AsyncStorage.setItem('@scores', JSON.stringify(scoresData)).then(
+            console.log('Initialized Local Storage scores with a loss')
+          );
+        }
+      });
+    } else {
+      const ref = doc(firestore_db, 'scores', userInfo.uid);
+      const snap = await getDoc(ref);
+      if (!snap.exists()) {
+        setDoc(doc(firestore_db, 'scores', userInfo.uid), {
+          gamesPlayed: 1,
+          gamesWon: 0,
+          curStreak: 0,
+          largestStreak: 0,
+          guessDist: Array.from({ length: MAX_GUESSES }, () => 0),
+        });
+        console.log('Doc Created');
+      } else {
+        try {
+          await runTransaction(firestore_db, async (transaction) => {
+            const scoreDoc = await transaction.get(ref);
+            if (!scoreDoc.exists()) {
+              console.log('ERROR: Scores document does not exit for this user');
+            }
+            const newGamesPlayed = scoreDoc.data().gamesPlayed + 1;
+            transaction.update(ref, {
+              gamesPlayed: newGamesPlayed,
+              curStreak: 0,
+            });
+          });
+          console.log('Transaction committed');
+        } catch (e) {
+          console.log('Transaction failed:', e);
+        }
+      }
+    }
+  }, [userInfo]);
+
   useEffect(() => {
     if (guesses[guesses.length - 1] === targetWord) {
       setGameStatus(STATUS_WIN);
-      dispatch(addWin({ guesses: guesses.length }));
+      addWin(guesses.length);
     } else if (guesses.length === MAX_GUESSES) {
       setGameStatus(STATUS_LOSS);
-      dispatch(addLoss());
+      addLoss();
     }
-  }, [guesses, dispatch, targetWord]);
+  }, [guesses, addLoss, addWin, targetWord]);
 
   const newGame = () => {
     dispatch(resetGuesses());
